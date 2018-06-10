ccee3ef09578dc76f3559823586aaa4802b30f05
786213750 <786213750g@gmail.com>
2018-06-09 22:44:02 -0700

    update to cluster

---
+++ try/classification/Classification SVM.py
@@ -0,0 +1,30 @@
+import numpy as np
+from sklearn import preprocessing, cross_validation, neighbors,svm
+import pandas as pd
+import pickle
+
+
+df= pd.read_csv('breast-cancer-wisconsin.data.txt')
+df.replace("?",-99999, inplace=True)
+df.drop(['id'],1,inplace=True)
+
+X=np.array(df.drop(['class'],1))
+y=np.array(df['class'])
+
+X_train,X_test,Y_train,Y_test=cross_validation.train_test_split(X,y,test_size=0.2)
+
+clf = svm.SVC()
+clf.fit(X_train,Y_train)
+#with open('classificationKN','wb') as f:
+ #   pickle.dump(clf,f)
+#pickle_in= open('classificationKN','rb')
+#clf=pickle.load(pickle_in)
+accuracy =clf.score(X_test,Y_test)
+#print(accuracy)
+ex_1=np.array([[1,2,3,1,2,1,4,4,3],[7,6,5,9,1,9,4,4,1]])
+ex_2=np.array([4,1,3,1,2,1,4,4,3])
+#print(ex_2)
+#ex_2.reshape(1,-1)
+#print(ex_2)
+predict_value=clf.predict(ex_1)
+#print(predict_value,accuracy)
---
+++ try/clustering/clustering.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+from matplotlib import style
+style.use('ggplot')
+
+
+X=np.array([[1,2],
+            [1.5,1.8],
+            [5,8],
+            [8,8],
+            [1,0.6],
+            [9,11]])
+
+clf=KMeans(n_clusters=2)
+clf.fit(X)
+centroids = clf.cluster_centers_
+labels=clf.labels_
+colors=['g.','r.','c.','b.','k.','o.']
+print(centroids)
+for i in range(len(X)):
+    plt.plot(X[i][0],X[i][1],colors[labels[i]], markersize=10)
+
+plt.scatter(centroids[:,0],centroids[:,1],marker='x',s=10)
+plt.show()
