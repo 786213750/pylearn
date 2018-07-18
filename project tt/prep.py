dc71b3d5db7df8f48b1612731b8df52e6ab1f080
Rai-2018 <--w.liang1009@gmail.com>
2018-07-18 15:56:52 -0700

    Wrong Branch

--- project tt/prep.py
+++ project tt/prep.py
@@ -5,7 +5,7 @@ import pandas as pd
 import numpy as np
 import os
 ##import image path for nn
-Train_path="c:\\thomas\\project tt\\t1"
+Train_path="C:\\Users\\Amos\\Downloads\\PortableGit\\pylearn\\project tt\\images"
 IMG_size= 50
 ## set up for dict
 s = pd.Series(list("abcdefghijklmnopqrstuvwxyz0123456789"+"abcdefghijklmnopqrstuvwxyz".upper()))
@@ -31,5 +31,44 @@ def create_train_data():
     return(training_data)
 create_train_data()
 
+n_classes = 62
+x = tf.placeholder('float', [None, 250])
+y = tf.placeholder(tf.float32, [None, n_classes])
 
-    
+
+
+def conv2d(x, W):
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+def maxpool2d(x):
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+
+def convolutional_neural_network(x):#, keep_rate):
+    weights = {
+        'W_conv1': tf.Variable(tf.random_normal([5, 5, 1, 32])),
+        'W_conv2': tf.Variable(tf.random_normal([5, 5, 32, 64])),
+        'W_fc': tf.Variable(tf.random_normal([12*12*64, 1024])),
+        'out': tf.Variable(tf.random_normal([1024, n_classes]))
+    }
+
+    biases = {
+        'b_conv1': tf.Variable(tf.random_normal([32])),
+        'b_conv2': tf.Variable(tf.random_normal([64])),
+        'b_fc': tf.Variable(tf.random_normal([1024])),
+        'out': tf.Variable(tf.random_normal([n_classes]))
+    }
+
+    x = tf.reshape(x, shape=[-1, 28, 28, 1])
+    conv1 = tf.nn.relu(conv2d(x, weights['W_conv1']) + biases['b_conv1'])
+    conv1 = maxpool2d(conv1)
+    conv2 = tf.nn.relu(conv2d(conv1, weights['W_conv2']) + biases['b_conv2'])
+    conv2 = maxpool2d(conv2)
+
+    fc = tf.reshape(conv2, [-1, 12*12*64])
+    fc = tf.nn.relu(tf.matmul(fc, weights['W_fc']) + biases['b_fc'])
+
+    output = tf.matmul(fc, weights['out']) + biases['out']
+    return output
+
+prediction = convolutional_neural_network(x)
