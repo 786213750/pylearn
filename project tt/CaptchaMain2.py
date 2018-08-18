38a4490b5e95f83ef7854f38d378684f08243cbc
Dreamle1 <35672312+Dreamle1@users.noreply.github.com>
2018-08-17 17:04:14 -0700

    a
    a

--- project tt/CaptchaMain2.py
+++ project tt/CaptchaMain2.py
@@ -13,7 +13,7 @@ path = pathname + "/images"
 
 #Main
 
-for _ in range(1):
+for _ in range(1000):
     #Create Captcha and write to Images Folder
     contours2 = []
     num_array = []
@@ -27,26 +27,25 @@ for _ in range(1):
     CaptchaFunctions.elimContours(contours,contours2,hierarchy)
     
     #Exception checker
-##    if (CharInCaptcha < len(contours2)):
-##        os.remove(os.path.join(path,StringName))
-##        
-##        continue
-##    try:
-##        for i in range(len(contours2)):
-##            (_,_,w,_) = cv2.boundingRect(contours2[i])
-##            if (w > 40):
-##                os.remove(os.path.join(path,StringName))
-##                print('lol')
-##                raise CaptchaFunctions.Continue1
-##    except CaptchaFunctions.Continue1:
-##        continue
+    if (CharInCaptcha < len(contours2)):
+        os.remove(os.path.join(path,StringName))
+        
+        continue
+    try:
+        for i in range(len(contours2)):
+            (_,_,w,_) = cv2.boundingRect(contours2[i])
+            if (w > 40):
+                os.remove(os.path.join(path,StringName))
+                print('lol')
+                raise CaptchaFunctions.Continue1
+    except CaptchaFunctions.Continue1:
+        continue
         
     
     coords = CaptchaFunctions.returncoords(contours2,num_array,String,returnImage,path)
     for i in range(len(coords)):
         tl = (coords[i][0],coords[i][1])
         br = (coords[i][0]+coords[i][2],coords[i][1]+coords[i][3])
-        
         Createxml.write_xml(path,os.path.join(path,String + '.png'),String + '.' + str(i) + '.png',[String[i]],[tl],[br],CaptchaFunctions.trypath(os.path.dirname(path) + "//xml"))
         
          
